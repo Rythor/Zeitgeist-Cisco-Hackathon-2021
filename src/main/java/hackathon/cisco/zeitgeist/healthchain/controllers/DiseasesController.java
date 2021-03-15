8176a49d18a72ba5420414cc12661bfc6c62504f
Jonah "Jon" Morgan <57231486+CypherCrow@users.noreply.github.com>
2021-03-14 23:39:48 -0700

    Updated Controller files and package name

---
+++ src/main/java/hackathon/cisco/zeitgeist/healthchain/controllers/DiseasesController.java
@@ -0,0 +1,10 @@
+package hackathon.cisco.zeitgeist.healthchain.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/diseases")
+public class DiseasesController {
+
+}
